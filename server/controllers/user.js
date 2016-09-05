f917767c8099fc87336a05e63df13d600e15d3a9
andela-jngatia <joan.ngatia@andela.com>
2016-09-05 14:25:11 +0300

    [Feature]Implement User deletion logic and routes

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -30,5 +30,22 @@ module.exports = {
                 return res.status(200).send(user)
             })
             .catch(error => res.status(400).send(error));
+    },
+    delete(req, res) {
+        return User
+            .findById(req.params.userId, {})
+            .then(user => {
+                if(!user){
+                    return res.status(404).send({
+                        message: 'User does not exist',
+                    })
+                }
+                return user
+                    .destroy()
+                    .then(() => res.status(204).send())
+                    .catch(error => res.status(400).send(error));
+
+            })
+            .catch(error => res.status(400).send(error));
     }
 }
--- server/routes/index.js
+++ server/routes/index.js
@@ -7,4 +7,5 @@ module.exports = (app) => {
     app.post('/api/users', usersController.create);
     app.get('/api/users', usersController.list);
     app.get('/api/users/:userId', usersController.retrieve);
+    app.delete('/api/users/:userId', usersController.delete);
 }
